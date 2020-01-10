087299324326b007d4aaa37dd16fa43d4be0b7f2
locdandcoded <53569883+locdandcoded@users.noreply.github.com>
2020-01-10 10:49:52 -0600

    created develop folder, and added suedo code to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-
+// What is needed to make this employee template engine???
+// User story: I am a development team director, utilizeing this template Engine, so that I can add members to team and see the full staff in a centralized location.
+// I need to build a commmand line app  in node to generate a team directory
+// The user will be prompted  for user information
+//          *employee name
+//          *employee title
+//          *employee id
+//          *employee email
+//          *employee office number
+//          *employee github id
+// If the user is entering a intern the user will be prompted for
+//          *intern name
+//          *intern title
+//          *intern id
+//          *intern email
+//          *intern office number
+//          *intern school
+// After every employee is added the user will be asked if they would like to add more employees and if they are done
+// Once done the information will be apended to a card and a index.html file will be generated to be opened
+//When the user opens index.html in browser the user will see the newly created team 
+// 
